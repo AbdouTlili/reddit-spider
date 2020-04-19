9df80543146057ca9683547de71cce2a7b6b7972
AbdouTlili <ha_tlili@esi.dz>
2020-04-19 22:49:06 +0100

    adding beautifulSoup and python-twitter the the requirments

--- bot.py
+++ bot.py
@@ -6,9 +6,3 @@ url ='https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg'
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
 # Ignore SSL certificate errors
 
-req = urllib.request.Request(headers=headers, url=url)
-
-image = urllib.request.urlopen(req).read()
-
-with open('img.png','wb') as f:
-    f.write(image)
